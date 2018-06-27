096361fa1b2bd73f658de754e752e21e9d8bc1c4
JeromyIrvine <jer@carbonated.io>
2018-06-27 15:55:59 -0400

    Consolidated value conversion logic and added ValueTypeMapper for framework types.

--- src/Carbonated.Data/MapperCollection.cs
+++ src/Carbonated.Data/MapperCollection.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 
 namespace Carbonated.Data
@@ -102,73 +101,31 @@ namespace Carbonated.Data
 
         private void AddFrameworkTypes()
         {
-            mappers.Add(new TypeMapper<bool>(GetValue<bool>));
-            mappers.Add(new TypeMapper<byte>(GetValue<byte>));
-            mappers.Add(new TypeMapper<short>(GetValue<short>));
-            mappers.Add(new TypeMapper<int>(GetValue<int>));
-            mappers.Add(new TypeMapper<long>(GetValue<long>));
-            mappers.Add(new TypeMapper<float>(GetValue<float>));
-            mappers.Add(new TypeMapper<double>(GetValue<double>));
-            mappers.Add(new TypeMapper<decimal>(GetValue<decimal>));
-            mappers.Add(new TypeMapper<DateTime>(GetValue<DateTime>));
-            mappers.Add(new TypeMapper<Guid>(GetGuid));
-            mappers.Add(new TypeMapper<char>(GetChar));
-            mappers.Add(new TypeMapper<string>(GetValue<string>));
-            mappers.Add(new TypeMapper<byte[]>(GetValue<byte[]>));
+            mappers.Add(new ValueTypeMapper<bool>());
+            mappers.Add(new ValueTypeMapper<byte>());
+            mappers.Add(new ValueTypeMapper<short>());
+            mappers.Add(new ValueTypeMapper<int>());
+            mappers.Add(new ValueTypeMapper<long>());
+            mappers.Add(new ValueTypeMapper<float>());
+            mappers.Add(new ValueTypeMapper<double>());
+            mappers.Add(new ValueTypeMapper<decimal>());
+            mappers.Add(new ValueTypeMapper<DateTime>());
+            mappers.Add(new ValueTypeMapper<Guid>());
+            mappers.Add(new ValueTypeMapper<char>());
+            mappers.Add(new ValueTypeMapper<string>());
+            mappers.Add(new ValueTypeMapper<byte[]>());
 
-            mappers.Add(new TypeMapper<bool?>(GetNullableValue<bool?>));
-            mappers.Add(new TypeMapper<byte?>(GetNullableValue<byte?>));
-            mappers.Add(new TypeMapper<short?>(GetNullableValue<short?>));
-            mappers.Add(new TypeMapper<int?>(GetNullableValue<int?>));
-            mappers.Add(new TypeMapper<long?>(GetNullableValue<long?>));
-            mappers.Add(new TypeMapper<float?>(GetNullableValue<float?>));
-            mappers.Add(new TypeMapper<double?>(GetNullableValue<double?>));
-            mappers.Add(new TypeMapper<decimal?>(GetNullableValue<decimal?>));
-            mappers.Add(new TypeMapper<DateTime?>(GetNullableValue<DateTime?>));
-            mappers.Add(new TypeMapper<Guid?>(GetNullableGuid));
-            mappers.Add(new TypeMapper<char?>(GetNullableChar));
-        }
-
-        private T GetValue<T>(IDataRecord record) 
-            => record.IsDBNull(0) ? default(T) : (T)Convert.ChangeType(record.GetValue(0), typeof(T));
-
-        private T GetNullableValue<T>(IDataRecord record) 
-            => record.IsDBNull(0) ? default(T) : (T)Convert.ChangeType(record.GetValue(0), Nullable.GetUnderlyingType(typeof(T)));
-
-        private Guid GetGuid(IDataRecord record)
-        {
-            // If we're using SQL Server and the Guid is backed by a UniqueIdentifier
-            // column, we can use GetValue. If it's backed by a string, we need to parse it
-            // directly because ChangeType doesn't have a string => Guid conversion.
-            if (record.GetFieldType(0) == typeof(Guid))
-            {
-                return GetValue<Guid>(record);
-            }
-            return record.IsDBNull(0) || string.IsNullOrEmpty(record.GetString(0)) ? Guid.Empty : Guid.Parse(record.GetString(0));
-        }
-
-        private Guid? GetNullableGuid(IDataRecord record)
-        {
-            // See note in GetGuid for details about parsing string values. We also need to
-            // ensure that DbNull gets converted to null.
-            if (record.GetFieldType(0) == typeof(Guid))
-            {
-                return record.IsDBNull(0) ? null : (Guid?)record.GetGuid(0);
-            }
-            return record.IsDBNull(0) || string.IsNullOrEmpty(record.GetString(0)) ? null : (Guid?)Guid.Parse(record.GetString(0));
-        }
-
-        private char GetChar(IDataRecord record)
-        {
-            // The SqlServer record treats all CHAR columns as strings, so we need to get
-            // char values as strings.
-            return record.IsDBNull(0) || string.IsNullOrEmpty(record.GetString(0)) ? default(char) : record.GetString(0)[0];
-        }
-
-        private char? GetNullableChar(IDataRecord record)
-        {
-            // Same basic logic as GetChar, but returning null instead of default for nulls.
-            return record.IsDBNull(0) || string.IsNullOrEmpty(record.GetString(0)) ? null : (char?)record.GetString(0)[0];
+            mappers.Add(new ValueTypeMapper<bool?>());
+            mappers.Add(new ValueTypeMapper<byte?>());
+            mappers.Add(new ValueTypeMapper<short?>());
+            mappers.Add(new ValueTypeMapper<int?>());
+            mappers.Add(new ValueTypeMapper<long?>());
+            mappers.Add(new ValueTypeMapper<float?>());
+            mappers.Add(new ValueTypeMapper<double?>());
+            mappers.Add(new ValueTypeMapper<decimal?>());
+            mappers.Add(new ValueTypeMapper<DateTime?>());
+            mappers.Add(new ValueTypeMapper<Guid?>());
+            mappers.Add(new ValueTypeMapper<char?>());
         }
     }
 }
---
+++ src/Carbonated.Data/ValueTypeMapper`1.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Carbonated.Data
+{
+    /// <summary>
+    /// Maps and converts the first column of a record to a framework value type using 
+    /// <see cref="ValueConverter.ToType{T}(object)"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the value that will be returned. This is expected to be one of the
+    /// framework value types, string, or byte[].</typeparam>
+    public class ValueTypeMapper<T> : TypeMapper<T>
+    {
+        /// <summary>
+        /// Constructs a Type Mapper and sets the creator function.
+        /// </summary>
+        public ValueTypeMapper() : base(ConvertValue) { }
+
+        private static T ConvertValue(IDataRecord record) => ValueConverter.ToType<T>(record.GetValue(0));
+    }
+}
